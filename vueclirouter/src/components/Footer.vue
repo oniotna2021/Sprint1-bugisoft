88b288a3810833241a13e284a56b235145af55f5
dayanatru28 <dayantru28@gmail.com>
2021-10-06 19:54:37 -0500

    Creacion de Proyecto con VueCli

---
+++ vueclirouter/src/components/Footer.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <footer class="bg-dark text-center text-lg text-light">
+      <div class="container p-4">
+        <div class="row">
+          <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+            <h5 class="text-uppercase">Informacion de la página</h5>
+            <p>
+              Como empresa de desarrollo web nos dedicamos a ofrecer asesorias
+              en tu pagina web como estructura HTML, diseño CSS y javaScript,
+              contomas con diferentes diseños base de paginas para que no
+              empieces desde cero, ademas de eso te estaremos mostrando las
+              ultimas tendicias en desarrollo web.
+            </p>
+          </div>
+
+          <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+            <h5 class="text-uppercase">Contacto</h5>
+            <div class="row">
+              <div class="col lg-6 text-start">
+                <p>
+                  Miembro: Antonio Jose Mogollon Miranda<br />
+                  Cargo: Scrum Master
+                </p>
+                <p>
+                  Miembro: Leidy Dayana Ortega<br />
+                  Cargo: Desarollador
+                </p>
+                <p>
+                  Miembro: Cristian Camilo Reyes<br />
+                  Cargo: Desarollador
+                </p>
+
+                <p>
+                  Miembro: William Guillermo Mendoza Mojica<br />
+                  Cargo: Desarollador
+                </p>
+
+                <p>
+                  Miembro: Ruber Hernan Sanchez<br />
+                  Cargo: Desarollador
+                </p>
+              </div>
+              <div class="col lg-6 text-end">
+                <p>
+                  Celular: 321 725 8986<br />
+                  Email:
+                  <a
+                    href="mailto:tonyusb@gmail.com?Subject=Interesado%20sobre%20las%20noticias%20web"
+                    class="text-light"
+                    >tonyusb@gmail.com</a
+                  >
+                </p>
+
+                <p>
+                  Celular: 322 330 1797<br />
+                  Email:
+                  <a
+                    href="mailto:dayanatru28@gmail.com?Subject=Interesado%20sobre%20las%20noticias%20web"
+                    class="text-light"
+                    >dayanatru28@gmail.com</a
+                  >
+                </p>
+
+                <p>
+                  Celular: 311 849 1797<br />
+                  Email:
+                  <a
+                    href="mailto:camiloreyessena@gmail.com?Subject=Interesado%20sobre%20las%20noticias%20web"
+                    class="text-light"
+                    >camiloreyessena@gmail.com</a
+                  >
+                </p>
+
+                <p>
+                  Celular: 318 572 3561<br />
+                  Email:
+                  <a
+                    href="mailto:william.mendoza@ustabuca.edu.co?Subject=Interesado%20sobre%20las%20noticias%20web"
+                    class="text-light"
+                    >william.mendoza@ustabuca.edu.co</a
+                  >
+                </p>
+
+                <p>
+                  Celular: ###########<br />
+                  Email: --------
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        <!--<img src="./src/img/github-logo_icon-icons.com_73546.png" /><a
+          class="text-light text-center"
+          href="https://github.com/oniotna2021/Sprint1-bugisoft-src.git"
+          target="_blank"
+        >
+          Ir al repositorio GitHub</a
+        > 
+        -->
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Footer'
+};
+</script>
---
+++ vueclirouter/src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import DetallesServicios from '../views/DetallesServicios.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/detallesServicios',
+    name: 'detallesServicios',
+    component: () => import(/* webpackChunkName: "about" */ '../views/DetallesServicios.vue')
+  },
+ 
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
